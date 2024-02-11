718a1e798a12f03d3aa7f5a37bb9eee05bdb5e85
Nicolas Tinjaca <nicolas.tinjaca@uptc.edu.co>
2024-02-10 20:34:44 -0500

    Deleting unuseful code

--- src/main/java/org/example/Runner.java
+++ src/main/java/org/example/Runner.java
@@ -11,32 +11,6 @@ import java.util.List;
 
 public class Runner {
     public static void main(String[] args) {
-        // Creamos una instancia del controlador de relaciones
-
-        RelationShipController controller = new RelationShipController();
-
-        // Recuperamos el grafo
-        Graph<NPC, DefaultWeightedEdge> graph = controller.getGraph();
-
-
-        if ( controller.findNPC("Amelia") != null && controller.findNPC("Pekora") != null) {
-            // Calculamos el camino más corto entre los dos NPC
-            List<NPC> shortestPath = controller.calculateShortestPath("Amelia", "Pekora");
-
-            if (shortestPath != null) {
-                // Imprimimos el camino más corto
-                System.out.println("Camino más corto entre " + "Amelia" + " y " + "Pekora" + ":");
-                for (NPC npc : shortestPath) {
-                    System.out.println(npc.getName());
-                }
-            } else {
-                System.out.println("No se encontró un camino entre los NPC.");
-            }
-        } else {
-            System.out.println("No se encontraron NPC con los nombres especificados.");
-        }
-
-
 
         Window window = new Window();
     }
